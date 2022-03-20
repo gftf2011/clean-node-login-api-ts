70b091a7be83cc3e5bfc1743ecbf269cc71ae994
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-03-20 18:22:10 -0300

    feat: add DbDirector

---
+++ src/infrastructure/helpers/clients/builders/db-director.ts
@@ -0,0 +1,25 @@
+/**
+ * Domain | Data
+ */
+import { IDbClientBuilder, IDbDirector } from '@/data/protocols/db/builder'
+
+export class DbDirector implements IDbDirector {
+  private builder?: IDbClientBuilder
+
+  /**
+   *
+   * @param builder
+   */
+  public setBuilder (builder: IDbClientBuilder): void {
+    this.builder = builder
+  }
+
+  public build (): void {
+    this.builder?.setDb()
+    this.builder?.setHost()
+    this.builder?.setMax()
+    this.builder?.setPass()
+    this.builder?.setPort()
+    this.builder?.setUser()
+  }
+}
