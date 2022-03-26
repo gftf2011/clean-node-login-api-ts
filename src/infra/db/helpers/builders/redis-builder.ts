c3d6ce653905c5299cd118b16d4f0d19c8bf2133
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-03-26 20:43:58 -0300

    feat: create builder for redis client

---
+++ src/infra/db/helpers/builders/redis-builder.ts
@@ -0,0 +1,52 @@
+/**
+ * Driver
+ */
+import redis from 'redis'
+
+/**
+  * Domain | Data
+  */
+import { IDbClientBuilder } from '@/infra/contracts'
+
+export class RedisClientBuilder implements IDbClientBuilder {
+  private host?: string
+
+  private port?: number
+
+  private user?: string
+
+  private pass?: string
+
+  private db?: string
+
+  public setHost (): void {
+    this.host = process.env.REDIS_HOST
+  }
+
+  public setPort (): void {
+    const entryPort = process.env.REDIS_PORT
+    this.port = entryPort ? +entryPort : 6379
+  }
+
+  public setUser (): void {
+    this.user = process.env.REDIS_USER
+  }
+
+  public setPass (): void {
+    this.pass = process.env.REDIS_PASSWORD
+  }
+
+  public setDb (): void {
+    this.db = process.env.REDIS_DB_NUMBER
+  }
+
+  public setMax (): void {}
+
+  public build (): any {
+    const result = redis.createClient({
+      url: `redis://${this.user}:${this.pass}@${this.host}:${this.port}/${this.db}`,
+      enable_offline_queue: false
+    })
+    return result
+  }
+}
