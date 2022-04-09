977f27ffa379af7520b1cbae8923c37f940fcde0
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-04-09 15:52:10 -0300

    feat: create express route adapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,13 @@
+import { HttpRequest } from '@/presentation/ports'
+import { Request, Response } from 'express'
+import { WebController } from '@/presentation/controllers'
+
+export const adaptRoute = (controller: WebController) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
