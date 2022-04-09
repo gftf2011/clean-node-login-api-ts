00739c5c827c409d6b97b0f55dddd0957f578c26
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-04-09 17:23:29 -0300

    feat: create cluster

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,6 +5,14 @@ import { loader } from '@/main/loader'
 
 import { server } from '@/main/server'
 
-loader().then(() => {
-  server.listen(process.env.PORT, () => console.log(`Server running at http://localhost:${process.env.PORT}`))
-}).catch(console.error)
+import cluster from 'cluster'
+import os from 'os'
+import process from 'process'
+
+if (cluster.isPrimary) {
+  os.cpus().forEach(() => cluster.fork())
+} else {
+  loader().then(() => {
+    server.listen(process.env.PORT, () => console.log(`Server ${process.pid} running at http://localhost:${process.env.PORT}`))
+  }).catch(console.error)
+}
