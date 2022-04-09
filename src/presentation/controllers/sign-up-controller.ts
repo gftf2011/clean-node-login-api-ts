5029207ef3bc702c302b35620655ea7070ca7f3b
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-04-09 15:43:15 -0300

    feat: create sign up controller

---
+++ src/presentation/controllers/sign-up-controller.ts
@@ -0,0 +1,29 @@
+/**
+ * Use Cases
+ */
+import { ISignUpUseCase } from '@/use-cases/ports'
+
+/**
+ * Presentation
+ */
+import { HttpResponse, HttpRequest, ControllerOperation } from '@/presentation/ports'
+import { created, badRequest } from '@/presentation/helpers/http-helper'
+
+export class SignUpController implements ControllerOperation {
+  readonly requiredParams = ['email', 'password', 'taxvat', 'name', 'lastname']
+  private readonly signUpUseCase: ISignUpUseCase
+
+  constructor (signUpUseCase: ISignUpUseCase) {
+    this.signUpUseCase = signUpUseCase
+  }
+
+  async operation (request: HttpRequest): Promise<HttpResponse> {
+    const response = await this.signUpUseCase.perform(request.body)
+
+    if (response.isRight()) {
+      return created(response.value)
+    }
+
+    return badRequest(response.value)
+  }
+}
