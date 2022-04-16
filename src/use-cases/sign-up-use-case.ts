d4b6321df2d41b3a5227afcc7781759514f0d5a5
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-04-16 09:48:41 -0300

    feat: change system business rule in sign up use case

--- src/use-cases/sign-up-use-case.ts
+++ src/use-cases/sign-up-use-case.ts
@@ -2,7 +2,7 @@
  * Shared
  */
 import { Either, left, right } from '@/shared'
-import { UserAlreadyExistsError } from '@/shared/errors'
+import { UserAlreadyExistsError, ServerError } from '@/shared/errors'
 
 /**
  * Entites
@@ -12,42 +12,39 @@ import { UserEntity } from '@/entities'
 /**
  * Use Cases
  */
-import { IUserRepository, UserDto, BasicUserDto, ISignUpUseCase, ITokenService, SaltType } from '@/use-cases/ports'
+import { IUserRepository, UserDto, BasicUserDto, ISignUpUseCase, ITokenService, IEncryptService } from '@/use-cases/ports'
 
 export class SignUpUseCase implements ISignUpUseCase {
   constructor (
     private readonly userRepository: IUserRepository,
-    private readonly tokenService: ITokenService
+    private readonly tokenService: ITokenService,
+    private readonly cryptoEncryptService: IEncryptService
   ) {}
 
   async perform (request: BasicUserDto): Promise<Either<Error, UserDto>> {
+    if (!process.env.CODE_SALT) {
+      return left(new ServerError())
+    }
     const { email, password, lastname, name, taxvat } = request
     const userOrError: Either<Error, UserEntity> = UserEntity.create(name, lastname, taxvat, email, password)
     if (userOrError.isLeft()) {
       return left(userOrError.value)
     }
-    const userExists = !!this.userRepository.findUserByEmail(userOrError.value.getEmail())
-    if (!userExists) {
+    const userExists = await this.userRepository.findUserByEmail(this.cryptoEncryptService.encode(userOrError.value.getEmail()))
+    if (userExists) {
       return left(new UserAlreadyExistsError())
     }
-    const customSaltOrError = this.tokenService.salt(SaltType.CUSTOM)
-    if (customSaltOrError.isLeft()) {
-      return left(customSaltOrError.value)
-    }
-    const defaultSaltOrError = this.tokenService.salt(SaltType.DEFAULT)
-    if (defaultSaltOrError.isLeft()) {
-      return left(defaultSaltOrError.value)
-    }
+    const customSalt = `${userOrError.value.getEmail()}${userOrError.value.getTaxvat()}`
+    const defaultSalt = process.env.CODE_SALT
     // encrypt password with user custom salt hash value
-    const hashedPassword = this.tokenService.encode(password, customSaltOrError.value)
+    const hashedPassword = this.tokenService.encode(password, customSalt)
     // encrypt encrypted password with code default salt hash value
-    const strongHashedPassword = this.tokenService.encode(hashedPassword, defaultSaltOrError.value)
+    const strongHashedPassword = this.tokenService.encode(hashedPassword, defaultSalt)
     const user: UserDto = {
-      email,
-      name,
-      lastname,
-      taxvat,
-      salt: customSaltOrError.value,
+      email: this.cryptoEncryptService.encode(userOrError.value.getEmail()),
+      name: this.cryptoEncryptService.encode(userOrError.value.getName()),
+      lastname: this.cryptoEncryptService.encode(userOrError.value.getLastname()),
+      taxvat: this.cryptoEncryptService.encode(userOrError.value.getTaxvat()),
       password: strongHashedPassword
     }
     return right(await this.userRepository.create(user))
