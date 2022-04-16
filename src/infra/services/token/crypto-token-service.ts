09e2ee481b98fd70a7c3288d0be3a44ce3fc7bb6
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-04-16 09:46:56 -0300

    feat: change token service implementation

--- src/infra/services/token/crypto-token-service.ts
+++ src/infra/services/token/crypto-token-service.ts
@@ -3,34 +3,15 @@ import crypto from 'crypto'
 /**
  * Use Cases
  */
-import { ITokenService, SaltType } from '@/use-cases/ports'
+import { ITokenService } from '@/use-cases/ports'
 
-/**
- * Shared
- */
-import { Either, right, left } from '@/shared'
-import { ServerError } from '@/shared/errors'
-
-const CRYPTOGRAPHY_TYPE: string = 'sha512'
-const HASH_SIZE: number = 256
+const CIPHER: string = 'sha512'
 const ENCODED_PASSWORD_SIZE: number = 256
 const ENCODED_ITERATIONS = 50000
 
 export class CryptoTokenService implements ITokenService {
   private generateToken (password: string, salt: string): string {
-    return crypto.pbkdf2Sync(password, salt, ENCODED_ITERATIONS, ENCODED_PASSWORD_SIZE, CRYPTOGRAPHY_TYPE).toString('hex')
-  }
-
-  salt (type: SaltType): Either<Error, string> {
-    switch (type) {
-      case SaltType.CUSTOM:
-        if (!process.env.CODE_SALT_HASH) {
-          return left(new ServerError())
-        }
-        return right(process.env.CODE_SALT_HASH)
-      default:
-        return right(crypto.randomBytes(HASH_SIZE).toString('hex'))
-    }
+    return crypto.pbkdf2Sync(password, salt, ENCODED_ITERATIONS, ENCODED_PASSWORD_SIZE, CIPHER).toString('hex')
   }
 
   encode (password: string, salt: string): string {
--- src/use-cases/ports/token-service.ts
+++ src/use-cases/ports/token-service.ts
@@ -1,15 +1,4 @@
-/**
- * Shared
- */
-import { Either } from '@/shared'
-
-export enum SaltType {
-  CUSTOM = 0,
-  DEFAULT = 1
-}
-
 export interface ITokenService {
-  salt: (type: SaltType) => Either<Error, string>
   encode: (password: string, salt: string) => string
   compare: (password: string, salt: string, hashedPassword: string) => boolean
 }
