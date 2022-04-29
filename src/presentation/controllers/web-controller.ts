e35b461be2d31f5e31587770496871aabb7e39f9
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-04-29 05:04:53 -0300

    docs: add comments to WebController

--- src/presentation/controllers/web-controller.ts
+++ src/presentation/controllers/web-controller.ts
@@ -9,6 +9,11 @@ import { MissingParamsError } from '@/shared/errors'
 import { HttpResponse, HttpRequest, ControllerOperation } from '@/presentation/ports'
 import { badRequest, serverError } from '@/presentation/helpers/http-helper'
 
+/**
+ * @author Gabriel Ferrari Tarallo Ferraz <gftf2011@gmail.com>
+ * @desc Contains the base algorithm that handles the data presentation.
+ * It uses the {@link https://refactoring.guru/pt-br/design-patterns/template-method Template Method} design pattern
+ */
 export class WebController {
   private readonly controllerOperation: ControllerOperation
 
@@ -16,18 +21,30 @@ export class WebController {
     this.controllerOperation = controllerOperation
   }
 
+  /**
+   * @desc handles the user input
+   * @param {HttpRequest} request - data input which comes from the `client`
+   * @returns {Promise<HttpResponse>} data output after operation
+   */
   public async handle (request: HttpRequest): Promise<HttpResponse> {
     try {
       const missingParams: string[] = WebController.getMissingParams(request, this.controllerOperation.requiredParams)
       if (missingParams.length !== 0) {
         return badRequest(new MissingParamsError(missingParams))
       }
-      return await this.controllerOperation.operation(request)
+      const response = await this.controllerOperation.operation(request)
+      return response
     } catch (error) {
       return serverError(error as Error)
     }
   }
 
+  /**
+   * @desc check which parameters are missing
+   * @param {HttpRequest} request - data input which comes from the `client`
+   * @param {string[]} requiredParams - required parameters which must be sent
+   * @returns {string[]} array of missing parameters
+   */
   public static getMissingParams (request: HttpRequest, requiredParams: string[]): string[] {
     const missingParams: string[] = []
     requiredParams.forEach((name) => {
