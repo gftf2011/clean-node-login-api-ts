111d347a8f8cd662fdc3750e3055ed5abf4b1952
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-05-21 18:34:58 -0300

    feat: add refresh token dao to repository

--- src/infra/repositories/user-repository.ts
+++ src/infra/repositories/user-repository.ts
@@ -1,22 +1,26 @@
 /**
  * Use Cases
  */
-import { IUserRepository, UserDto as User } from '@/use-cases/ports'
+import { IUserRepository, UserDto as User, RefreshTokenDto as RefreshToken } from '@/use-cases/ports'
 
 /**
  * Infrastructure
  */
-import { IUserDao } from '@/infra/contracts'
+import { IUserDao, IRefreshTokenDao } from '@/infra/contracts'
 
 export class UserRepository implements IUserRepository {
   private readonly userDAO: IUserDao
+  private readonly refreshTokenDAO: IRefreshTokenDao
 
-  constructor (userDAO: IUserDao) {
+  constructor (userDAO: IUserDao, refreshTokenDAO: IRefreshTokenDao) {
     this.userDAO = userDAO
+    this.refreshTokenDAO = refreshTokenDAO
   }
 
-  async create (user: User): Promise<User> {
-    return await this.userDAO.create(user)
+  async create (user: User, refreshToken: RefreshToken): Promise<User> {
+    const refreshTokenResponse = await this.refreshTokenDAO.create(refreshToken)
+    const userResponse = await this.userDAO.create(user, refreshTokenResponse.id)
+    return userResponse
   }
 
   async findUserByEmail (email: string): Promise<User> {
