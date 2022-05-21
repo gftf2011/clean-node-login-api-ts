5c2f18920c2b94f11d88aa50dea0e0db30e85842
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-05-21 18:15:30 -0300

    feat: update db transaction interface

--- src/infra/contracts/db-transaction.ts
+++ src/infra/contracts/db-transaction.ts
@@ -2,7 +2,7 @@ export interface DbTransaction {
   client: () => Promise<any>
   openTransaction: (client: any) => Promise<void>
   transaction: (client: any, statement: string, values: any[]) => Promise<any>
-  closeTransaction: (client: any) => Promise<void> | void
+  closeTransaction: (client: any) => Promise<void>
   commit: (client: any) => Promise<void>
   rollback: (client: any) => Promise<void>
 }
