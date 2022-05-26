1f2ef0079422176291764ea7597610df5c60e335
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-05-26 18:39:48 -0300

    feat: delete unused file

--- src/presentation/ports/controller-operation.ts
+++
@@ -1,8 +0,0 @@
-import { HttpRequest } from './http-request'
-import { HttpResponse } from './http-response'
-
-export interface ControllerOperation {
-  readonly requiredParams: string[]
-  readonly requiredHeaderParams: string[]
-  operation: (request: HttpRequest) => Promise<HttpResponse>
-}
