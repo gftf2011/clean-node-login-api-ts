7221fc18a371b4aead4c375319361e283ddad988
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-05-30 21:00:09 -0300

    docs: delete comments

--- scripts/dev/postgres/init.sh
+++ scripts/dev/postgres/init.sh
@@ -73,27 +73,6 @@ psql clean_node_login_api_ts_postgres_dev_db -c "CREATE TABLE IF NOT EXISTS user
   )
 )"
 
-# Create Function to encrypt data
-# ========================================
-# psql clean_node_login_api_ts_postgres_dev_db -c "CREATE OR REPLACE FUNCTION encrypt_users_email () RETURNS TRIGGER AS \$\$
-#   BEGIN
-#     IF (TG_OP = 'INSERT') THEN
-#       UPDATE users_schema.users
-#       SET email = encrypt(NEW.email, NEW.taxvat)
-#       WHERE id = NEW.id;
-#     END IF;
-#     RETURN NEW;
-#   END;
-# \$\$ LANGUAGE plpgsql"
-# ========================================
-
-# Create Trigger to encrypt email
-# ========================================
-# psql clean_node_login_api_ts_postgres_dev_db -c "
-#   CREATE TRIGGER encrypt_users_email_trigger AFTER INSERT ON users_schema.users FOR EACH ROW EXECUTE PROCEDURE encrypt_users_email()
-# "
-# ========================================
-
 # Create Index for gmail like index domains
 psql clean_node_login_api_ts_postgres_dev_db -c "CREATE INDEX IF NOT EXISTS idx_users_email_gmail ON users_schema.users(email) WHERE email LIKE '%gmail.com%'"
 psql clean_node_login_api_ts_postgres_dev_db -c "CREATE INDEX IF NOT EXISTS idx_users_email_outlook ON users_schema.users(email) WHERE email LIKE '%outlook.com%'"
