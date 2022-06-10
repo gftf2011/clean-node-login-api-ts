28dc3cf99cb4c2944aa05a06f4168c7fd3a8231f
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-06-10 08:23:15 -0300

    feat: update access token dao

--- backend/src/infra/contracts/access-token-dao.ts
+++ backend/src/infra/contracts/access-token-dao.ts
@@ -5,5 +5,7 @@ import { AccessTokenDto as AccessToken } from '@/use-cases/ports'
 
 export interface IAccessTokenDao {
   create: (accessToken: string) => Promise<AccessToken>
+  deleteById: (id: string) => Promise<AccessToken>
+  updateToken: (accessTokenId: string, token: string) => Promise<AccessToken>
 }
  
--- backend/src/infra/dao/access-token-dao.ts
+++ backend/src/infra/dao/access-token-dao.ts
@@ -23,5 +23,31 @@ export class AccessTokenDao implements IAccessTokenDao {
     }
     return parsedResponse
   }
+
+  async deleteById (id: string): Promise<AccessToken> {
+    const statement: string = 'DELETE FROM users_schema.access_token WHERE id = $1 RETURNING *'
+
+    const values: any[] = [id]
+
+    const response = await this.dbClientManager.query(statement, values)
+    const parsedResponse: AccessToken = {
+      id: response.rows[0].id,
+      token: response.rows[0].token
+    }
+    return parsedResponse
+  }
+
+  async updateToken (accessTokenId: string, token: string): Promise<AccessToken> {
+    const statement: string = 'UPDATE users_schema.access_token SET token = $1 WHERE id = $2 RETURNING *'
+
+    const values: any[] = [token, accessTokenId]
+
+    const response = await this.dbClientManager.query(statement, values)
+    const parsedResponse: AccessToken = {
+      id: response.rows[0].id,
+      token: response.rows[0].token
+    }
+    return parsedResponse
+  }
 }
  
