e29b889ff1fc375e9988f4dc657dd397967aae8d
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-06-22 23:15:31 -0300

    feat: add mongodb

---
+++ scripts/dev/mongo/init.sh
@@ -0,0 +1,39 @@
+set -e
+
+mongo << EOF
+  db = db.getSiblingDB('clean_node_login_api_ts_mongo_dev_db')
+
+  db.createCollection('users')
+
+  db.createRole(
+    {
+      role: 'dev-role',
+      privileges: [
+        {
+          resource: {
+            db: 'clean_node_login_api_ts_mongo_dev_db',
+            collection: 'users'
+          },
+          actions: [ 'find', 'insert' ]
+        },
+      ],
+      roles: [
+        { role: 'readWrite', db: 'clean_node_login_api_ts_mongo_dev_db' }
+      ]
+    },
+    { 
+      w: 'majority',
+      wtimeout: 5000
+    }
+  )
+
+  db.createUser({
+    user: 'dev_user',
+    pwd: 'dev_user',
+    mechanisms: [ 'SCRAM-SHA-256' ],
+    roles: [{
+      role: 'dev-role',
+      db: 'clean_node_login_api_ts_mongo_dev_db'
+    }]
+  })
+EOF
