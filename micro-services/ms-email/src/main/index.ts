4b680d6596bda9c0aa583458371d43620bfbebb9
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-06-25 22:22:29 -0300

    feat: create loader

--- micro-services/ms-email/src/main/index.ts
+++ micro-services/ms-email/src/main/index.ts
@@ -1,11 +1,15 @@
 import '../main/bootstrap'
 
+import { loader } from '../main/loader'
+
 import { server } from '../main/server'
 
-server()
-  .then(() => {
-    console.log(
-      `Queue consumer listening at ${process.env.RABBITMQ_CONNECTION_URL}`
-    )
-  })
-  .catch(console.error)
+loader().then(() => {
+  server()
+    .then(() => {
+      console.log(
+        `Queue consumer listening at ${process.env.RABBITMQ_CONNECTION_URL}`
+      )
+    })
+    .catch(console.error)
+})
---
+++ micro-services/ms-email/src/main/loader/index.ts
@@ -0,0 +1,5 @@
+import { MongoDbClientPool } from '../../infra/db'
+
+export const loader = async (): Promise<any> => {
+  await MongoDbClientPool.connect()
+}
