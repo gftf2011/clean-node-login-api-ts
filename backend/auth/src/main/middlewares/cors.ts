b50b94f30432690635fd55bcd109a74a59c31667
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-07-08 12:21:01 -0300

    feat: add cors white list protection

--- backend/auth/src/main/middlewares/cors.ts
+++ backend/auth/src/main/middlewares/cors.ts
@@ -5,8 +5,8 @@ export const cors = (
   res: Response,
   next: NextFunction
 ): void => {
-  res.set('access-control-allow-origin', '*')
+  res.set('access-control-allow-origin', `${process.env.CLIENT_ORIGIN_URL}`)
   res.set('access-control-allow-headers', '*')
-  res.set('access-control-allow-methods', '*')
+  res.set('access-control-allow-methods', 'POST')
   next()
 }
--- backend/email/src/main/adapters/rabbitmq-consume-message-adapter.ts
+++ backend/email/src/main/adapters/rabbitmq-consume-message-adapter.ts
@@ -9,11 +9,6 @@ import { Channel, ConsumeMessage } from 'amqplib'
 import { Controller, Request } from '../../presentation/ports'
 
 /**
- * Shared
- */
-import { Error500 } from '../../shared/errors'
-
-/**
  * @author Gabriel Ferrari Tarallo Ferraz <gftf2011@gmail.com>
  * @desc Rabbit MQ message consumer adapter
  * It uses the {@link https://refactoring.guru/pt-br/design-patterns/adapter Adapter} design pattern
@@ -30,16 +25,12 @@ export const adaptConsumeMessage = (
     if (msg !== null) {
       request.content = JSON.parse(msg.content.toString())
     }
-
-    const response = await controller.handle(request)
-    if (response instanceof Error500) {
-      channel.nack(msg, false, true)
-      return
-    }
-    if (response) {
-      channel.ack(msg, false)
-      return
-    }
-    channel.nack(msg, false, false)
+    /**
+     * Command below used to tell RabbitMQ to not dispatch new messages to work queues
+     * until de previous one is acknowledge
+     */
+    await channel.prefetch(1)
+    await controller.handle(request)
+    channel.ack(msg, false)
   }
 }
