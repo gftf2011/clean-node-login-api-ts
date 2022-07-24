366658ead922089bf686149886dcf0a0bacd4649
Gabriel Ferraz <gabriel.ferraz@midway.com.br>
2022-07-24 20:30:49 -0300

    test: update controller test suite

--- backend/auth/tests/unit/presentation/controllers/template-method/sign-up-controller.spec.ts
+++ backend/auth/tests/unit/presentation/controllers/template-method/sign-up-controller.spec.ts
@@ -1,8 +1,14 @@
+/* eslint-disable sort-imports */
+/**
+ * Driver
+ */
+import { cpf } from 'cpf-cnpj-validator';
+import faker from 'faker';
+
 /**
  * Presentation
  */
-import { Controller } from '../../../../../src/presentation/ports';
-// eslint-disable-next-line sort-imports
+import { Controller, HttpRequest } from '../../../../../src/presentation/ports';
 import {
   SignUpController,
   WebController,
@@ -17,6 +23,11 @@ import { ISignUpUseCase } from '../../../../../src/use-cases/ports';
  * Dummies
  */
 import { SignUpUseCaseDummy } from '../../../doubles/dummies';
+
+/**
+ * Shared
+ */
+import { MissingParamsError } from '../../../../../src/shared/errors';
 
 // eslint-disable-next-line no-shadow
 enum SIGN_UP_USE_CASE_TYPE {
@@ -49,6 +60,111 @@ describe('Sign-Up Controller', () => {
 
   it('should extend WebController', () => {
     expect(sut).toBeInstanceOf(WebController);
+  });
+
+  it('should return missing param error if email request parameter is missing', async () => {
+    const request: HttpRequest = {
+      body: {
+        password: faker.internet.password(),
+        taxvat: cpf.generate(),
+        name: faker.name.firstName(),
+        lastname: faker.name.lastName(),
+      },
+      headers: {
+        host: faker.internet.ip(),
+      },
+    };
+
+    const response = await sut.handle(request);
+
+    expect(response).toEqual({
+      statusCode: 400,
+      body: new MissingParamsError(['email']),
+    });
+  });
+
+  it('should return missing param error if password request parameter is missing', async () => {
+    const request: HttpRequest = {
+      body: {
+        email: faker.internet.email(),
+        taxvat: cpf.generate(),
+        name: faker.name.firstName(),
+        lastname: faker.name.lastName(),
+      },
+      headers: {
+        host: faker.internet.ip(),
+      },
+    };
+
+    const response = await sut.handle(request);
+
+    expect(response).toEqual({
+      statusCode: 400,
+      body: new MissingParamsError(['password']),
+    });
+  });
+
+  it('should return missing param error if taxvat request parameter is missing', async () => {
+    const request: HttpRequest = {
+      body: {
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        name: faker.name.firstName(),
+        lastname: faker.name.lastName(),
+      },
+      headers: {
+        host: faker.internet.ip(),
+      },
+    };
+
+    const response = await sut.handle(request);
+
+    expect(response).toEqual({
+      statusCode: 400,
+      body: new MissingParamsError(['taxvat']),
+    });
+  });
+
+  it('should return missing param error if name request parameter is missing', async () => {
+    const request: HttpRequest = {
+      body: {
+        taxvat: cpf.generate(),
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        lastname: faker.name.lastName(),
+      },
+      headers: {
+        host: faker.internet.ip(),
+      },
+    };
+
+    const response = await sut.handle(request);
+
+    expect(response).toEqual({
+      statusCode: 400,
+      body: new MissingParamsError(['name']),
+    });
+  });
+
+  it('should return missing param error if name request parameter is missing', async () => {
+    const request: HttpRequest = {
+      body: {
+        taxvat: cpf.generate(),
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        name: faker.name.firstName(),
+      },
+      headers: {
+        host: faker.internet.ip(),
+      },
+    };
+
+    const response = await sut.handle(request);
+
+    expect(response).toEqual({
+      statusCode: 400,
+      body: new MissingParamsError(['lastname']),
+    });
   });
 
   afterEach(() => {
